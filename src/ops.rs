364f8e9fc18751e046102504fa8d92f129bd689b
peterthenelson <nelson.peter.a@gmail.com>
2025-05-01 23:09:18 -0400

    More ops

--- src/ops.rs
+++ src/ops.rs
@@ -1,26 +1,64 @@
+pub fn xop<const N: usize, F: Fn(i32, i32) -> i32>(reg: &mut [i32; N], a_x: usize, a_y: usize, b_x: usize, b_y: usize, c_x: usize, c_y: usize, f: F) {
+    reg[c_x % N] = f(reg[a_x % N] ^ reg[a_y % N], reg[b_x % N] ^ reg[b_y % N]) ^ reg[c_y % N];
+}
 
 pub fn add<const N: usize>(reg: &mut [i32; N], a_x: usize, a_y: usize, b_x: usize, b_y: usize, c_x: usize, c_y: usize) {
-    reg[c_x % N] = (
-        (reg[a_x % N] ^ reg[a_y % N]) + (reg[b_x % N] ^ reg[b_y % N])
-    ) ^ reg[c_y % N];
+    xop(reg, a_x, a_y, b_x, b_y, c_x, c_y, |x, y| x + y);
+}
+
+pub fn sub<const N: usize>(reg: &mut [i32; N], a_x: usize, a_y: usize, b_x: usize, b_y: usize, c_x: usize, c_y: usize) {
+    xop(reg, a_x, a_y, b_x, b_y, c_x, c_y, |x, y| x - y);
+}
+
+pub fn mul<const N: usize>(reg: &mut [i32; N], a_x: usize, a_y: usize, b_x: usize, b_y: usize, c_x: usize, c_y: usize) {
+    xop(reg, a_x, a_y, b_x, b_y, c_x, c_y, |x, y| x * y);
+}
+
+pub fn div<const N: usize>(reg: &mut [i32; N], a_x: usize, a_y: usize, b_x: usize, b_y: usize, c_x: usize, c_y: usize) {
+    xop(reg, a_x, a_y, b_x, b_y, c_x, c_y, |x, y| x / y);
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    fn setup_reg<const N: usize>(x: i32, y: i32, z: i32) -> [i32; N] {
+        assert!(N >= 6, "Register array must be at least 6 elements long");
+        let mut reg = [0; N];
+        reg[0] = 0x7ABC1234 ^ x;
+        reg[1] = 0x7ABC1234;
+        reg[2] = 0x1234ABCD ^ y;
+        reg[3] = 0x1234ABCD;
+        reg[4] = 0x1337BEEF ^ z;
+        reg[5] = 0x1337BEEF;
+        reg
+    }
+
     #[test]
     fn test_add() {
-        let mut reg = [0; 16];
-        reg[0] = 0x7ABC1234 ^ 222;
-        reg[1] = 0x7ABC1234;
-        reg[2] = 0x1234ABCD ^ 333;
-        reg[3] = 0x1234ABCD;
-        reg[4] = 0x1337BEEF ^ 100;
-        reg[5] = 0x1337BEEF;
-        println!("Before: {:?}", reg);
+        let mut reg: [i32; 8] = setup_reg(222, 333, 100);
         add(&mut reg, 0, 1, 2, 3, 4, 5);
-        println!("After:  {:?}", reg);
         assert_eq!(reg[4], 0x1337BEEF ^ 555);
+    }
+
+    #[test]
+    fn test_sub() {
+        let mut reg: [i32; 8] = setup_reg(222, 333, 100);
+        sub(&mut reg, 0, 1, 2, 3, 4, 5);
+        assert_eq!(reg[4], 0x1337BEEF ^ -111);
+    }
+
+    #[test]
+    fn test_mul() {
+        let mut reg: [i32; 8] = setup_reg(222, 333, 100);
+        mul(&mut reg, 0, 1, 2, 3, 4, 5);
+        assert_eq!(reg[4], 0x1337BEEF ^ (222 * 333));
+    }
+
+    #[test]
+    fn test_div() {
+        let mut reg: [i32; 8] = setup_reg(222, 333, 100);
+        div(&mut reg, 0, 1, 2, 3, 4, 5);
+        assert_eq!(reg[4], 0x1337BEEF ^ (222 / 333));
     }
 }
