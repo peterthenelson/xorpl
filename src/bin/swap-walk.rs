59ef1921e5b30d42973f1b3d938e3df3350f1e62
peterthenelson <nelson.peter.a@gmail.com>
2025-05-03 23:29:42 -0400

    Fix the swap-walk to actually find some paths

--- src/bin/swap-walk.rs
+++ src/bin/swap-walk.rs
@@ -30,22 +30,35 @@ fn has_plaintext<const R: usize, const C: usize>(x: &XMatrix<R, C>) -> bool {
     false
 }
 
-fn min_max_row_sum<const R: usize, const C: usize>(x: &XMatrix<R, C>) -> (u8, u8) {
-    assert!(C < 256, "C must be less than 256");
-    let mut min: u8 = C as u8;
-    let mut max: u8 = 0;
-    for i in 0..R {
-        let sum: u8 = x.row(i).sum();
-        min = min.min(sum);
-        max = max.max(sum);
+fn valid_end_state<const R: usize, const C: usize>(x: &XMatrix<R, C>) -> bool {
+    assert!(C % 2 == 0, "C (num cipher/key registers) must be even");
+    // A subset of rows form a permutation of the default layout's first C rows.
+    let mut indicator_rows: [u8; C] = [0; C];
+    for r in 0..R {
+        let mut sum = 0;
+        let mut first: usize = 0;
+        for c in 0..C {
+            if x.get(r, c) == 1 {
+                sum += 1;
+                if first == 0 {
+                    first = c;
+                }
+            }
+        }
+        if sum == 1 {
+            indicator_rows[first] = 1;
+        }
     }
-    (min, max)
+    indicator_rows.iter().all(|&x| x == 1)
 }
 
 fn neighbors<const R: usize, const C: usize>(x: &XMatrix<R, C>) -> Vec<XMatrix<R, C>> {
     let mut neighbors = Vec::new();
-    for i in 0..(R - 1) {
-        for j in i..R {
+    for i in 0..R {
+        for j in 0..R {
+            if i == j {
+                continue;
+            }
             let mut y = x.clone();
             y.add_row(i, j);
             if y.rank() != x.rank() || has_plaintext(&y) {
@@ -63,12 +76,12 @@ pub fn main() {
     const C: usize = 6;
     let mut x = default_layout::<R, C>();
     assert_eq!(x.rank(), C);
-    assert_eq!(min_max_row_sum(&x).1, 1);
+    assert!(valid_end_state(&x));
     let mut seen: HashSet<XMatrix<R, C>> = HashSet::new();
     let mut end_states: HashSet<XMatrix<R, C>> = HashSet::new();
     let mut rng = rand::rng();
-    for _ in 0..1000000 {
-        if !seen.contains(&x) && min_max_row_sum(&x).1 == 1 {
+    for _ in 0..10000000 {
+        if !seen.contains(&x) && valid_end_state(&x) {
             println!("Reached valid end-state:\n{}", x.serialize());
             end_states.insert(x);
         }
